12946f1bbc8cf5ebc2384a22d5895415aa1978c6
James <thejamesm@users.noreply.github.com>
2024-06-13 11:31:51 +0100

    Initial getter/setter version
    Incomplete - need to resume step 20

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from room import Room
 
 kitchen = Room("kitchen")
 kitchen.set_description("A dank and dirty room buzzing with flies")
-#kitchen.description = "A dank and dirty room buzzing with flies"
 ballroom = Room("ballroom")
 ballroom.set_description("A vast room with a shiny wooden floor")
-#ballroom.description = "A vast room with a shiny wooden floor"
 dining_hall = Room("dining hall")
 dining_hall.set_description("A large room with ornate golden decorations")
-#dining_hall.description = "A large room with ornate golden decorations"
 
 kitchen.link_room(dining_hall, "south")
 dining_hall.link_room(kitchen, "north")
--- room.py
+++ room.py
@@ -1,9 +1,7 @@
 class Room:
     def __init__(self, room_name):
         self.set_name(room_name)
-        # self.name = room_name
         self.set_description(None)
-        # self.description = None
         self.linked_rooms = {}
 
     def get_name(self):
@@ -12,28 +10,17 @@ class Room:
     def set_name(self, new_name):
         self.__name = new_name
 
-    # @property
-    # def name(self):
-    #     return self.__name
-
     def get_description(self):
         return self.__description
 
     def set_description(self, new_description):
         self.__description = new_description
 
-    # @property
-    # def description(self):
-    #     return self.__description
-
     def describe(self):
         print(self.get_description())
-
-    # def describe(self):
-    #     print(self.description)
 
     def link_room(self, room_to_link, direction):
         self.linked_rooms[direction] = room_to_link
 
     def get_details(self):
-        print(self.name)
+        print(self.get_name())
