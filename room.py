0ae0acd6d3cdcd80eef0137795214d68422bdd8d
James <thejamesm@users.noreply.github.com>
2024-06-13 18:01:32 +0100

    Add linked room methods
    - Getter `@property` for the now private attribute
    - Class static attribute listing the cardinal directions and their
      opposite counterparts
    - `two_way_link()` method to create symmetrical links between rooms
      (facilitated by the cardinal direction dict)

--- room.py
+++ room.py
@@ -1,4 +1,11 @@
 class Room:
+    dir_opposites = {
+        "north": "south",
+        "east": "west",
+        "south": "north",
+        "west": "east"
+    }
+
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
@@ -23,12 +30,20 @@ class Room:
     def describe(self):
         print(self.description)
 
+    @property
+    def linked_rooms(self):
+        return self.__linked_rooms
+
     def link_room(self, room_to_link, direction):
         self.__linked_rooms[direction] = room_to_link
+
+    def two_way_link(self, room_to_link, direction):
+        self.link_room(room_to_link, direction)
+        room_to_link.link_room(self, Room.dir_opposites[direction])
 
     def get_details(self):
         print(self.name)
         print("-------------------------")
         print(self.description)
-        for direction, room in self.__linked_rooms.items():
+        for direction, room in self.linked_rooms.items():
             print(f"The {room.name} is {direction}")
