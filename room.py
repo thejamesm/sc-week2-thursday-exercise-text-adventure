fda0974f3e51a5ad38cf0dfcea4d7a2d77648028
James <thejamesm@users.noreply.github.com>
2024-06-13 11:38:38 +0100

    Initial property decorator version
    As with other branch, incomplete at step 20

--- room.py
+++ room.py
@@ -1,36 +1,27 @@
 class Room:
     def __init__(self, room_name):
-        self.set_name(room_name)
-        # self.name = room_name
-        self.set_description(None)
-        # self.description = None
+        self.name = room_name
+        self.description = None
         self.linked_rooms = {}
 
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
 
-    def set_name(self, new_name):
+    @name.setter
+    def name(self, new_name):
         self.__name = new_name
 
-    # @property
-    # def name(self):
-    #     return self.__name
-
-    def get_description(self):
+    @property
+    def description(self):
         return self.__description
 
-    def set_description(self, new_description):
+    @description.setter
+    def description(self, new_description):
         self.__description = new_description
 
-    # @property
-    # def description(self):
-    #     return self.__description
-
     def describe(self):
-        print(self.get_description())
-
-    # def describe(self):
-    #     print(self.description)
+        print(self.description)
 
     def link_room(self, room_to_link, direction):
         self.linked_rooms[direction] = room_to_link
