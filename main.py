0640f45b050d56c243d02dd699c9cd229e1647fb
James <thejamesm@users.noreply.github.com>
2024-06-17 02:42:07 +0100

    Task 3: Add stealing and non-enemy character
    - Characters can carry an item of `Item` class
    - Carried item can be stolen, or dropped when defeated in combat
    - Player automatically collects dropped items
    - Player has a list of carried items

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from room import Room
 from item import Item
-from character import Enemy
+from character import Character, Enemy
+
+player_items = []
 
 kitchen = Room("kitchen")
 kitchen.description = "A dank and dirty room buzzing with flies"
@@ -14,11 +16,18 @@ dining_hall.link_room(kitchen, "north")
 dining_hall.link_room(ballroom, "west")
 ballroom.link_room(dining_hall, "east")
 
+cheese = Item("cheese", "A pungent block of cheese")
+
 dave = Enemy("Dave", "A smelly zombie")
 dave.conversation = "Brrlgrh... rgrhl... brains..."
 dave.weakness = "cheese"
 
+catrina = Character("Catrina", "A friendly skeleton")
+catrina.conversation = "Hi, how are you?"
+catrina.item = cheese
+
 dining_hall.character = dave
+ballroom.character = catrina
 
 current_room = kitchen
 while True:
@@ -43,13 +52,24 @@ while True:
         if inhabitant:
             print("What will you fight with?")
             combat_item = input("> ")
-            if inhabitant.fight(combat_item):
-                current_room.character = None
+            combat_result, dropped_item = inhabitant.fight(combat_item)
+            if combat_result:
+                if isinstance(inhabitant, Enemy):
+                    current_room.character = None
+                    if dropped_item:
+                        player_items.append(dropped_item)
             else:
                 print()
                 print("~~~ You lose! ~~~")
                 print()
                 exit()
+        else:
+            print("There's nobody here")
+    elif command == "steal":
+        if inhabitant:
+            stolen_item = inhabitant.steal()
+            if stolen_item:
+                player_items.append(stolen_item)
         else:
             print("There's nobody here")
     elif command in ("quit", "exit"):
